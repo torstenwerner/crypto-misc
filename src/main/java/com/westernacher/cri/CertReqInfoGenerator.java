262e92afe7fb2783c06d7e316d7e43978af1b1f3
Torsten Werner <torsten.werner@westernacher.com>
2020-03-11 00:10:40 +0100

    fix publicKey handling

--- src/main/java/com/westernacher/cri/CertReqInfoGenerator.java
+++ src/main/java/com/westernacher/cri/CertReqInfoGenerator.java
@@ -1,11 +1,9 @@
 package com.westernacher.cri;
 
 import org.bouncycastle.asn1.ASN1InputStream;
-import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1Primitive;
 import org.bouncycastle.asn1.pkcs.CertificationRequestInfo;
 import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.util.io.pem.PemReader;
 
@@ -37,20 +35,11 @@ public class CertReqInfoGenerator {
     }
 
     private void newRequestInfo(byte[] publicKey, String x500Name, String outputFile) throws IOException {
-        final SubjectPublicKeyInfo publicKeyInfo = subjectPublicKeyInfo(publicKey);
+        final SubjectPublicKeyInfo publicKeyInfo = SubjectPublicKeyInfo.getInstance(publicKey);
         final X500Name x500 = new X500Name(x500Name);
         final CertificationRequestInfo requestInfo = new CertificationRequestInfo(x500, publicKeyInfo, null);
         try (final OutputStream outputStream = new FileOutputStream(outputFile)) {
             outputStream.write(requestInfo.getEncoded());
         }
-    }
-
-    /**
-     * Transforms a DER encoded RSA public key into a {@link SubjectPublicKeyInfo} object.
-     */
-    private SubjectPublicKeyInfo subjectPublicKeyInfo(byte[] publicKey) {
-        final ASN1ObjectIdentifier rsaIdentifier = new ASN1ObjectIdentifier("1.2.840.113549.1.1.1");
-        final AlgorithmIdentifier rsaAlgorithm = new AlgorithmIdentifier(rsaIdentifier);
-        return new SubjectPublicKeyInfo(rsaAlgorithm, publicKey);
     }
 }
